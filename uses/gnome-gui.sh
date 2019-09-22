f23d2445d400c8ef57814e00e896eefae78d7e7c
Liam Dawson <liam@ldaws.com>
2019-09-22 12:55:31 +1000

    Capslock as esc in Gnome

--- uses/gnome-gui.sh
+++ uses/gnome-gui.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# arc-darker theme
 dconf write /org/gnome/desktop/interface/gtk-theme "'Arc-Darker'"
+# use capslock as esc
+dconf write /org/gnome/desktop/input-sources/xkb-options "['terminate:ctrl_alt_bksp', 'caps:escape']"
 
