f566ca084f13f633dbd9bfc5a53b72b0f0a8e6e8
Liam Dawson <liam@ldaws.com>
2020-01-06 10:57:33 +1100

    Add ave and avl aliases

--- stowed/fish/functions/aws-vault.fish
+++ stowed/fish/functions/aws-vault.fish
@@ -22,3 +22,36 @@ if command -v aws-vault >/dev/null
     complete "-C$parts[2]"
   end
 end
+
+if command -v aws-vault >/dev/null
+  function ave
+    set -l aws_profile $argv[1]
+    set -e argv[1]
+
+    aws-vault exec $aws_profile -- $argv
+  end
+
+  function avl
+    set -l aws_profile $argv[1]
+    set -e argv[1]
+
+    aws-vault login $aws_profile
+  end
+
+  function __fish_aws_vault_complete_second_token_onwards
+    set -l current_line (commandline --current-process --cut-at-cursor)
+    set -l tokens (commandline --current-process --tokenize)
+
+    set -l parts (string split --max 1 $tokens[2] -- $current_line)
+
+    complete "-C$parts[2]"
+  end
+
+  complete -ec 'ave'
+  complete -ec 'avl'
+
+  complete -c ave -n '__fish_is_first_arg' -xa '(aws-vault list --profiles)'
+  complete -c avl -n '__fish_is_first_arg' -xa '(aws-vault list --profiles)'
+  complete -c ave -n 'not __fish_is_first_arg' -xa '(__fish_aws_vault_complete_second_token_onwards)'
+  complete -c avl -n 'not __fish_is_first_arg' -xa '(__fish_aws_vault_complete_second_token_onwards)'
+end
