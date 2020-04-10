92af9c30d66fffbb5337ee5e3d877723c71b3976
Liam Dawson <liam@ldaws.com>
2020-04-10 17:57:33 +1000

    Configure Linux GUI settings

--- uses/gnome-gui.sh
+++ uses/gnome-gui.sh
@@ -4,8 +4,10 @@ set -eu
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# arc-darker theme
-dconf write /org/gnome/desktop/interface/gtk-theme "'Arc-Darker'"
+dconf write /org/gnome/desktop/interface/gtk-theme "'QogirBudgie'"
+
+# # arc-darker theme
+# dconf write /org/gnome/desktop/interface/gtk-theme "'Arc-Darker'"
 # use capslock as esc
 dconf write /org/gnome/desktop/input-sources/xkb-options "['terminate:ctrl_alt_bksp', 'caps:escape']"
 
@@ -13,6 +15,9 @@ dconf write /org/gnome/desktop/interface/show-battery-percentage true
 
 # touchpad scroll direction
 dconf write /org/gnome/desktop/peripherals/touchpad/natural-scroll false
+
+# budgie stuff
+dconf write /org/ubuntubudgie/plugins/budgie-wpreviews/enable-previews true
 
 (
   cd ../dconf || exit
