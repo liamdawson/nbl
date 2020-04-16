d0859b3c5587c94d9a8b907b21f5b7bd5807ca5a
Liam Dawson <liam@ldaws.com>
2020-04-16 10:06:26 +1000

    Update global envs

--- uses/dev-envs.sh
+++ uses/dev-envs.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
-ruby_target_version="2.6.5"
-python2_target_version="2.7.16"
-python3_target_version="3.7.4"
-node_target_version="10.16.3"
+ruby_target_version="2.7.1"
+python2_target_version="2.7.17"
+python3_target_version="3.8.2"
+node_target_version="12.16.2"
+
+set -x
 
 rbenv install -s "$ruby_target_version"
 pyenv install -s "$python2_target_version"
 pyenv install -s "$python3_target_version"
 nodenv install -s "$node_target_version"
+
+rbenv global "$ruby_target_version"
+pyenv global "$python3_target_version" "$python2_target_version"
+nodenv global "$node_target_version"
